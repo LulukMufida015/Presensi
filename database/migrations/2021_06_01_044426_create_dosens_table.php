e971182fbe8586d5a0ff2faf72d44c897db04c8e
naily26 <nailymasruri@gmail.com>
2021-06-01 12:27:58 +0700

    Create MSDC Dosen+Matakuliah

---
+++ database/migrations/2021_06_01_044426_create_dosens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDosensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosen', function (Blueprint $table) {
+            $table->id();
+            $table->string('nip', 15);
+            $table->string('nama_dosen', 255)->nullable();
+            $table->integer('jenis_kelasmin')->nullable();
+            $table->string('no_handphone', 13)->nullable();
+            $table->string('alamat', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosens');
+    }
+}
